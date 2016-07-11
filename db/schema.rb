e2d56c7665fa055ccff5b13412e5776848b84742
Michael Pena Gaillard <mpgaillard@Michaels-MacBook-Pro-3.local>
2016-07-10 20:32:40 -0400

    adds remember me

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705163055) do
+ActiveRecord::Schema.define(version: 20160709224002) do
 
   create_table "tasks", force: :cascade do |t|
     t.integer  "time_limit",    limit: 4
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160705163055) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.string   "password_digest", limit: 255
+    t.string   "remember_digest", limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
