12c242b7ae6b6d07629bf2519da4874a009f4730
Michael Pena Gaillard <mpgaillard@Michaels-MacBook-Pro-3.local>
2016-11-02 20:02:28 -0400

    test stupid password

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '104.236.51.1', roles: [:web, :app, :db], primary: true
+server '104.236.51.1', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:mpgaillard/programming_class_judge.git'
 set :application,     'programming_class_judge'
